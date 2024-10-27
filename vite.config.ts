b3081b963e3c9ccdb0398b881ca8433f363fb5a2
avo <voanna08@gmail.com>
2024-10-27 12:32:22 -0500

    config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gio',
+  base: '/gio/',
   plugins: [react({ jsxImportSource: '@emotion/react' })],
 });
