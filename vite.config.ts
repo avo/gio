06cc6fc767d04fbc86d59c9bc0e64edca1190a53
avo <voanna08@gmail.com>
2024-10-27 13:44:06 -0500

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react({ jsxImportSource: '@emotion/react' })],
-// });
-
 export default defineConfig(({ command }) => {
   const config = {
     base: '/',
@@ -13,7 +8,7 @@ export default defineConfig(({ command }) => {
   };
 
   if (command !== 'serve') {
-    config.base = '/gio/';
+    config.base = 'https://annavo.dev';
   }
 
   return config;
