44ca96f24c00f2bfdac9a9125c57caf1106d41da
avo <voanna08@gmail.com>
2024-10-29 15:29:27 -0500

    fix react router scrolling

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,10 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import { PageWrapper } from '../layout/PageWrapper';
 
 export function Root() {
   return (
-    <PageWrapper>
-      <Outlet />
-    </PageWrapper>
+    <>
+      <ScrollRestoration />
+      <PageWrapper>
+        <Outlet />
+      </PageWrapper>
+    </>
   );
 }
